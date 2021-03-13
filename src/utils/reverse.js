b044c5660a522a144c7987072367ee306935bf74
Kirill Sklyarov <kirill_sklyarov@epam.com>
2021-03-13 20:06:07 +0300

    implement reverse from console

---
+++ src/utils/reverse.js
@@ -0,0 +1,11 @@
+function reverse(source) {
+  let reversed = '';
+
+  for (let i = source.length - 1; i >= 0; i--) {
+    reversed += source[i];
+  }
+
+  return reversed;
+}
+
+module.exports.reverse = reverse;
